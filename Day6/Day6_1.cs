72d5d73c179f63ae018885b679f930f1d4a9ae30
HugoEklund <eklund.hugo@gmail.com>
2025-12-11 16:14:15 +0100

    working on 6_1

--- Day6/Day6_1.cs
+++ Day6/Day6_1.cs
@@ -4,6 +4,39 @@ public static class Day6_1
 {
     public static void Exec()
     {
+        int totalSum = 0;
+        int[] sum = null;
+        int[] prod = null;
         
+        foreach (var row in File.ReadLines("Day6/input.txt"))
+        {
+            var cols = row.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            if (int.TryParse(cols[0], out _) == null)
+            {
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    int num = int.Parse(cols[i]);
+                    if (sum == null)
+                    {
+                        sum[i] = num;
+                        prod[i] = num;
+                    }
+                    else
+                    {
+                        sum[i] += num;
+                        prod[i] *= num;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < cols.Length; i++)
+                {
+                    totalSum += cols[i] == "+" ? sum[i] : prod[i];
+                }
+            }
+        }
+        Console.WriteLine(totalSum);
     }
 }
